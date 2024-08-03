b065d2147fb9c6b2081d27c30b6d5090785a4f32
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-03 07:01:05 +0530

    Create HashSet.java

---
+++ HashSet.java
@@ -0,0 +1,13 @@
+public class HashSet{
+  public static void main(String[] args) {
+Set <Person>per=new HashSet<>();
+   per.add(new Person(1, "Chanakya", 234, "Dev"));
+   per.add(new Person(5, "Chanakya", 234, "Dev"));
+   per.add(new Person(7, "Chanakya", 234, "Dev"));
+   per.add(new Person(9, "Chanakya", 234, "Dev"));
+
+   for (Person person : per) {
+    System.out.println(person);
+   } 
+  }
+}
